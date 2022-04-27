7f556d1aefe03578184942bb17008966048ee0e3
zeromountain <sonyeongsan@gmail.com>
2022-04-28 01:09:08 +0900

    💄 서비스 소개 추가

---
+++ client/src/components/Landing.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div className='h-60 bg-stone-700 text-white'>
+      <span>서비스 소개</span>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, sint
+        placeat quos maxime expedita dicta iure in ea dolore omnis. Sed,
+        quisquam ratione. Molestias architecto recusandae neque consequatur
+        atque aperiam?
+      </p>
+    </div>
+  );
+};
+
+export default Landing;
