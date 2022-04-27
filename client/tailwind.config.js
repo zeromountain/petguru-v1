59deea20d6eb6c859f7002b34bcdbb4bb3091cf6
zeromountain <sonyeongsan@gmail.com>
2022-04-27 14:53:42 +0900

    🔧  반응형 tailwindCSS config 수정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      mobile: '425px',
+      tablet: '768px',
+      desktop: '1024px',
+    },
     extend: {
       backgroundImage: {
         'bg-image': 'url("/src/assets/3.jpeg")',
