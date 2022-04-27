f8809aab289a6f62dfa6b126c000c14326dbc23e
zeromountain <sonyeongsan@gmail.com>
2022-04-27 14:54:07 +0900

    🧐  목데이터 추가

---
+++ client/src/data/index.ts
@@ -0,0 +1,77 @@
+type dataTypes = {
+  id: string;
+  title: string;
+  division: string;
+  comments: number;
+  views: number;
+  likes: number;
+};
+
+const data = [
+  {
+    id: 'asdfasdgag12314sdfgaxcv',
+    title: '산책 모임 하실 분 모집합니다.',
+    division: '산책벙개',
+    comments: 788,
+    views: 1000,
+    likes: 500,
+  },
+  {
+    id: 'asdgkljxcvlzxlcgjqouqqr',
+    title: '같이 강아지 훈련 연습하실 분 찾아요.',
+    division: '훈련벙개',
+    comments: 1,
+    views: 5,
+    likes: 1,
+  },
+  {
+    id: 'asdgkjasgkljso98qutiozx',
+    title: '강아지 이갈이 관련해서 질문드립니다.',
+    division: '고민상담',
+    comments: 1,
+    views: 5,
+    likes: 1,
+  },
+  {
+    id: 'qweirtouopaskgzxclxljsd',
+    title: '잃어버린 강아지를 찾습니다.',
+    division: '실종신고',
+    comments: 1,
+    views: 5,
+    likes: 1,
+  },
+  {
+    id: '1451235sdajsdgsdfsdfgsd',
+    title: '산책 모임 하실 분 모집합니다.',
+    division: '산책벙개',
+    comments: 1,
+    views: 5,
+    likes: 1,
+  },
+  {
+    id: '2350984936034sdfjsdagjlj',
+    title: '같이 강아지 훈련 연습하실 분 찾아요.',
+    division: '훈련벙개',
+    comments: 1,
+    views: 5,
+    likes: 1,
+  },
+  {
+    id: '1243235sdfjkasdlgoqwirtu',
+    title: '강아지 이갈이 관련해서 질문드립니다.',
+    division: '고민상담',
+    comments: 1,
+    views: 5,
+    likes: 1,
+  },
+  {
+    id: '857489571asdfkajfasdfASF',
+    title: '잃어버린 강아지를 찾습니다.',
+    division: '실종신고',
+    comments: 1,
+    views: 5,
+    likes: 1,
+  },
+];
+
+export default data;
