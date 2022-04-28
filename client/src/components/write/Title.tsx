4b08676200de015821eb7dd2da6db3b586d261a8
zeromountain <sonyeongsan@gmail.com>
2022-04-28 23:51:16 +0900

    💄 Title 컴포넌트 추가

---
+++ client/src/components/write/Title.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <input
+      type='text'
+      placeholder='제목을 입력하세요.'
+      className='w-full h-16 text-[3rem] font-bold text-black bg-transparent border-b-2 border-b-black  outline-0 mb-8 pb-[0.5rem]'
+    />
+  );
+};
+
+export default Title;
