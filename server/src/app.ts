6e359e22f56d208587f1e6077f1972ee7e126bcd
zeromountain <sonyeongsan@gmail.com>
2022-04-30 12:02:57 +0900

    👔  모듈 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,16 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 import connectDB from '../config/database';
+import auth from './routes/api/auth';
+import user from './routes/api/user';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
@@ -16,6 +21,9 @@ connectDB();
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+app.use('/api/auth', auth);
+app.use('/api/user', user);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
